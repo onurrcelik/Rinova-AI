3a3e99883aa9d3aa3fdaa3406253e10c35e762ff
onurrcelik <onur5celik8@gmail.com>
2025-12-18 19:32:47 +0100

    fix: export handlers from auth

--- auth.ts
+++ auth.ts
@@ -20,7 +20,7 @@ declare module 'next-auth' {
     }
 }
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [
         Credentials({
